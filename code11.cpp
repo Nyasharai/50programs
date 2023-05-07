cd5cea5cf19df1d59b693dec346c0778fe89bc22
Nyasharai <122803521+Nyasharai@users.noreply.github.com>
2023-05-07 23:02:10 +0530

    Add files via upload

---
+++ code11.cpp
@@ -0,0 +1,77 @@
+// singly linked list
+
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+
+void insertAtEnd(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void print(node *head)
+{
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+bool search(node *head, int key)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertAtEnd(head, 1);
+    insertAtEnd(head, 2);
+    insertAtEnd(head, 3);
+    insertAtHead(head, 4);
+    print(head);
+    cout << search(head, 5) << endl;
+    return 0;
+}
